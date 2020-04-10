d5f773bbe59d2aa899b3d220d299466f515c8063
Henry Moule <henry@care-planner.co.uk>
2020-04-10 14:23:46 +0100

    started implementing buttons

--- app/App.tsx
+++ app/App.tsx
@@ -3,16 +3,29 @@ import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Home from './Home';
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 
 const Stack = createStackNavigator();
 
+const theme = {
+  ...DefaultTheme,
+  roundness: 2,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: 'rgb(46,56,143)',
+    accent: 'rgb(235,50,35)',
+  },
+};
+
 const App: FunctionComponent = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="awal n-rbbi i-kraygatt ass" component={Home} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <PaperProvider theme={theme}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="awal n-rbbi i-kraygatt ass" component={Home} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
   );
 };
 
--- app/Home.tsx
+++ app/Home.tsx
@@ -1,34 +1,38 @@
 import React, { FunctionComponent, useEffect, useCallback, useState, useRef } from 'react';
-import { Alert, Linking, Text } from 'react-native';
+import { Alert, Linking, View, SafeAreaView } from 'react-native';
 import auth from '@react-native-firebase/auth';
 import storage from '@react-native-firebase/storage';
 import PushNotificationIOS from '@react-native-community/push-notification-ios';
 import PushNotification from 'react-native-push-notification';
 import Video from 'react-native-video';
 import moment from 'moment';
+import strings from './strings';
 import AsyncStorage from '@react-native-community/async-storage';
+import { Button } from 'react-native-paper';
 
 const Home: FunctionComponent = () => {
-  const [verse, setVerse] = useState('');
-  const [chapter, setChapter] = useState('');
+  const [verseUrl, setVerseUrl] = useState('');
+  const [chapterUrl, setChapterUrl] = useState('');
   const [versePaused, setVersePaused] = useState(true);
   const [chapterPaused, setChapterPaused] = useState(true);
   const verseRef = useRef<Video>();
   const chapterRef = useRef<Video>();
+  const date = moment().date();
+  const month = moment().month() + 1;
+  const verse = strings[1][date];
 
   const fetchVerse = useCallback(async () => {
-    const url = await storage().ref('/verses/1/1.mp3').getDownloadURL();
-    setVerse(url);
-  }, []);
+    const url = await storage().ref(`/verses/1/${date}.mp3`).getDownloadURL();
+    setVerseUrl(url);
+  }, [date]);
 
   const fetchChapter = useCallback(async () => {
-    const url = await storage().ref('/chapters/1/1.mp3').getDownloadURL();
-    setChapter(url);
-  }, []);
+    const url = await storage().ref(`/chapters/1/${date}.mp3`).getDownloadURL();
+    setChapterUrl(url);
+  }, [date]);
 
   useEffect(() => {
     auth().signInAnonymously();
-
     PushNotification.configure({
       onRegister: (token) => {
         console.log('TOKEN:', token);
@@ -67,27 +71,70 @@ const Home: FunctionComponent = () => {
 
   return (
     <>
-      {!!verse && (
+      {!!verseUrl && (
         <Video
           paused={versePaused}
           audioOnly={true}
-          source={{ uri: verse }} // Can be a URL or a local file.
+          source={{ uri: verseUrl }} // Can be a URL or a local file.
           ref={verseRef} // Store reference
           onBuffer={onBuffer} // Callback when remote video is buffering
           onError={onError} // Callback when video cannot be loaded
         />
       )}
-      {!!chapter && (
+      {!!chapterUrl && (
         <Video
           paused={chapterPaused}
           audioOnly={true}
-          source={{ uri: chapter }} // Can be a URL or a local file.
+          source={{ uri: chapterUrl }} // Can be a URL or a local file.
           ref={chapterRef} // Store reference
           onBuffer={onBuffer} // Callback when remote video is buffering
           onError={onError} // Callback when video cannot be loaded
         />
       )}
-      <Text onPress={() => Linking.openURL('http://www.tachelhit.info')}>kchem s-dar takat n-tgemmi-negh</Text>
+      <SafeAreaView style={{ flex: 1 }}>
+        <View style={{ flex: 1, justifyContent: 'flex-end', alignItems: 'center' }}>
+          {/* listen again to this verse */}
+          <Button
+            mode="contained"
+            icon="repeat"
+            uppercase={false}
+            style={{ margin: 10 }}
+            onPress={() => Linking.openURL('http://www.tachelhit.info')}
+          >
+            sfeld dagh i-tguri-ad
+          </Button>
+          {/* listen to the whole chapter */}
+          <Button
+            mode="contained"
+            icon="book"
+            uppercase={false}
+            style={{ margin: 10 }}
+            onPress={() => Linking.openURL('http://www.tachelhit.info')}
+          >
+            sfeld i-ugzzum-ad kullut
+          </Button>
+          {/* listen to all the Bible */}
+          <Button
+            mode="contained"
+            icon="book"
+            uppercase={false}
+            style={{ margin: 10 }}
+            onPress={() => Linking.openURL('http://www.tachelhit.info')}
+          >
+            sfeld i-warratn yadni n-sidi rbbi
+          </Button>
+          {/* enter our website: www.tachelhit.info */}
+          <Button
+            mode="contained"
+            icon="web"
+            uppercase={false}
+            style={{ margin: 10 }}
+            onPress={() => Linking.openURL('http://www.tachelhit.info')}
+          >
+            kchem s-dar takat n-tgemmi-negh
+          </Button>
+        </View>
+      </SafeAreaView>
     </>
   );
 };
