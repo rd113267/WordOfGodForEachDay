f7b826359200aa4873179643187b25b68ae338e2
Henry Moule <henry@care-planner.co.uk>
2020-05-08 17:41:03 +0100

    added code for bible sequence and updated notification

--- app/bibleRef.ts
+++ app/bibleRef.ts
@@ -1,7 +1,269 @@
 const bookInfo: { [key: number]: { length: number } } = {
   // Genesis
   1: {
-    length: 44,
+    length: 46,
+  },
+  // Exodus
+  2: {
+    length: 26,
+  },
+  // Leviticus
+  3: {
+    length: 13,
+  },
+  //  Numbers
+  4: {
+    length: 19,
+  },
+  // Deuteronomy
+  5: {
+    length: 19,
+  },
+  // Joshua
+  6: {
+    length: 18,
+  },
+  // Judges
+  7: {
+    length: 16,
+  },
+  // Ruth
+  8: {
+    length: 4,
+  },
+  // 1 Samuel
+  9: {
+    length: 22,
+  },
+  // 2 Samuel
+  10: {
+    length: 10,
+  },
+  // 1 Kings
+  11: {
+    length: 17,
+  },
+  // 2 Kings
+  12: {
+    length: 12,
+  },
+  // 1 Chronicles
+  13: {
+    length: 1,
+  },
+  // 2 Chronicles
+  14: {
+    length: 2,
+  },
+  // Ezra
+  15: {
+    length: 10,
+  },
+  // Nehemiah
+  16: {
+    length: 11,
+  },
+  // Esther
+  17: {
+    length: 10,
+  },
+  // Job
+  18: {
+    length: 30,
+  },
+  // Psalms
+  19: {
+    length: 48,
+  },
+  // Proverbs
+  20: {
+    length: 28,
+  },
+  // Ecclesiastes
+  21: {
+    length: 11,
+  },
+  // Song
+  22: {
+    length: 1,
+  },
+  // Isaiah
+  23: {
+    length: 38,
+  },
+  // Jeremiah
+  24: {
+    length: 26,
+  },
+  // Lamentations
+  25: {
+    length: 3,
+  },
+  // Ezekiel
+  26: {
+    length: 11,
+  },
+  // Daniel
+  27: {
+    length: 9,
+  },
+  // Hosea
+  28: {
+    length: 6,
+  },
+  // Joel
+  29: {
+    length: 1,
+  },
+  // Amos
+  30: {
+    length: 1,
+  },
+  // Obadiah
+  31: {
+    length: 1,
+  },
+  // Jonah
+  32: {
+    length: 4,
+  },
+  // Micah
+  33: {
+    length: 2,
+  },
+  // Nahum
+  34: {
+    length: 2,
+  },
+  // Habakkuk
+  35: {
+    length: 3,
+  },
+  // Zephaniah
+  36: {
+    length: 2,
+  },
+  // Haggai
+  37: {
+    length: 1,
+  },
+  // Zechariah
+  38: {
+    length: 5,
+  },
+  // Malachi
+  39: {
+    length: 2,
+  },
+  // N E W  T E S T A M E N T
+
+  // Matthew
+  40: {
+    length: 28,
+  },
+  // Mark
+  41: {
+    length: 16,
+  },
+  // Luke
+  42: {
+    length: 24,
+  },
+  // John
+  43: {
+    length: 21,
+  },
+  // Acts
+  44: {
+    length: 28,
+  },
+  // Romans
+  45: {
+    length: 16,
+  },
+  // 1 Corinthians
+  46: {
+    length: 16,
+  },
+  // 2 Corinthians
+  47: {
+    length: 13,
+  },
+  // Galatians
+  48: {
+    length: 6,
+  },
+  // Ephesians
+  49: {
+    length: 6,
+  },
+  // Philippians
+  50: {
+    length: 4,
+  },
+  // Colossians
+  51: {
+    length: 4,
+  },
+  // 1 Thessalonians
+  52: {
+    length: 8,
+  },
+  // 2 Thessalonians
+  53: {
+    length: 10,
+  },
+  // 1 Timothy
+  54: {
+    length: 6,
+  },
+  // 2 Timothy
+  55: {
+    length: 4,
+  },
+  // Titus
+  56: {
+    length: 13,
+  },
+  // Philemon
+  57: {
+    length: 5,
+  },
+  // Hebrews
+  58: {
+    length: 13,
+  },
+  // James
+  59: {
+    length: 7,
+  },
+  // 1 Peter
+  60: {
+    length: 5,
+  },
+  // 2 Peter
+  61: {
+    length: 22,
+  },
+  // 1 John
+  62: {
+    length: 5,
+  },
+  // 2 John
+  63: {
+    length: 1,
+  },
+  // 3 John
+  64: {
+    length: 1,
+  },
+  // Jude
+  65: {
+    length: 1,
+  },
+  // Revelation
+  66: {
+    length: 22,
   },
 };
 
